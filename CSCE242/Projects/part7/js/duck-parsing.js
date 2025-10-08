1f94778e8dad0641eb2598196393f998ebf33c7c
KMerced <kaylanyrmerced@proton.me>
2025-10-08 19:14:10 -0400

    fixed an error in the json

--- CSCE242/Projects/part7/js/duck-parsing.js
+++ CSCE242/Projects/part7/js/duck-parsing.js
@@ -13,20 +13,37 @@ const showDucks = async () => {
     const ducks = await getDucks();
 
     //Consts for all the duck info
-    const duckName = document.getElementById("duck_name");
+    //W3 schools was a godsend to help with this stuff, learned from here: 
+    //https://www.w3schools.com/jsref/prop_loc_search.asp
+
+    //For future reference, what this window.location.search thing does is that it takes the "?name=" parameter from each of the urls and stores it for future use, like in the duckName method after it!
+    const getStuff = window.location.search;
+    //Learned about the replace method from here:
+    //https://www.w3schools.com/jsref/jsref_replace.asp
+    //For future reference what replace does is that it just takes out the "?name=" so that when the rest of the code tries to find the duck the person clicked, it won't be wrong and mess itself up
+    const duckName = getStuff.replace("?name=", "")
+    const duckerName = document.getElementById("duck_name")
     const img = document.getElementById("img");
     const duckBrand = document.getElementById("brand");
     const duckLine = document.getElementById("line");
     const duckDate = document.getElementById("date");
     const story = document.getElementById("story-text");
 
+    let duck = null;
+    for(let i=0; i<ducks.length; i++) {
+        if(ducks[i].name == duckName) {
+            duck = ducks[i];
+            break;
+        }
+    }
+
     //Stuff to add everything
-    duckName.innerHTML = ducks.name;
-    img.src = duckBrand.img;
-    duckBrand.innerHTML = `Brand: ${ducks.brand}`;
-    duckLine.innerHTML = `Line: ${ducks.line}`;
-    duckDate.innerHTML = `Date Obtained: ${ducks.date}`;
-    story.innerHTML = ducks.story;
+    duckerName.innerHTML = duck.name;
+    img.src = `../../json/images/${duck.img}`;
+    duckBrand.innerHTML = `Brand: ${duck.brand}`;
+    duckLine.innerHTML = `Line: ${duck.line}`;
+    duckDate.innerHTML = `Date Obtained: ${duck.date}`;
+    story.innerHTML = duck.story;
 
 }
 
